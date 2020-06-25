f0eb7d2e7cadfcd83ac400e4ebfb5cd34a56cb29
Phan Dung Tri <phandungtri4work@gmail.com>
2020-06-25 15:28:56 +0700

    Add declaration file

---
+++ dist/index.d.ts
@@ -0,0 +1,4 @@
+declare const hexToRgb: (color: string) => string;
+declare const isDarkColor: (...args: Array<string | number>) => boolean;
+declare const randomHexColor: () => string;
+export { isDarkColor, hexToRgb, randomHexColor };
